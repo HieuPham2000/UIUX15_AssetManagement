ea69a5bd7d0de95ea9664d5f250ce3befa35b32b
HieuPham2000 <hieu.pt183535@gmail.com>
2022-01-19 21:57:25 +0700

    thêm constant cho popup

--- js/constant.js
+++ js/constant.js
@@ -2,5 +2,8 @@ const Constants = {
   SaveSuccess: "Lưu dữ liệu thành công!",
   AddSuccess: "Thêm mới dữ liệu thành công!",
   EditSuccess: "Cập nhật dữ liệu thành công!",
-  Validate: "Các trường bắt buộc không được phép để trống"
+  Validate: "Các trường bắt buộc không được phép để trống",
+  ConfirmBeforeCloseMsg: "Dữ liệu đã bị thay đổi, bạn có muốn lưu không?",
+  PopupHeaderGeneral: "Thông báo",
+  PopupHeaderDeleteRecord: "Xóa bản ghi"
 }
