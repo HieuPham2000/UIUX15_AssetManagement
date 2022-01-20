9e51b126c31a2dec3f7efc615b9eea0a5357bfe1
HieuPham2000 <hieu.pt183535@gmail.com>
2022-01-20 11:31:35 +0700

    cập nhật màn Đề nghị mua sắm

---
+++ data/manage-request-buy-asset-data.js
@@ -0,0 +1,13 @@
+const manageAssetBuyRequestData= [
+  {
+    id: 1,
+    code: "BR01",
+    assetName: "Ghế Xoay Hòa Phát",
+    price: "3.000.000",
+    createdDate: "10/01/2022",
+    createdBy: "Phạm Văn Thành",
+    manageBy: "Phạm Trung Hiếu",
+    status: "Chờ phê duyệt",
+    description: "Ghế ngồi thư giãn, có ngả lưng"
+  }
+];
