5b0f0cfa0c24a5a582817889a0f970df829c566c
HieuPham2000 <hieu.pt183535@gmail.com>
2022-01-20 11:05:08 +0700

    Sửa màn Quản lý đăng ký sử dụng

---
+++ data/manage-asset-use-request-data.js
@@ -0,0 +1,29 @@
+const manageAssetUseRequestData= [
+  {
+    id: 1,
+    code: "UR01",
+    assetCode: "TS01",
+    assetName: "Laptop Dell Latitude 7480",
+    createdDate: "10/01/2022",
+    dateFrom: "22/01/2022",
+    dateTo: "22/01/2025",
+    createdBy: "Phạm Hữu Anh Quốc",
+    manageBy: "Phạm Trung Hiếu",
+    status: "Chờ phê duyệt"
+  }
+];
+
+const manageAssetUseRequestEmptyObject= [
+  {
+    id: "",
+    code: "",
+    assetCode: "",
+    assetName: "",
+    createdDate: "",
+    dateFrom: "",
+    dateTo: "",
+    createdBy: "",
+    manageBy: "",
+    status: ""
+  }
+];
